b9c77e72a9b2a8f50262cfb9fec850a1e428af31
NelsonWamalwa <wamalwanelson@gmail.com>
2024-04-24 22:25:25 +0300

    Task 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+--A script that creates a table users with follwing fields
+-- id, email and name
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
+)
