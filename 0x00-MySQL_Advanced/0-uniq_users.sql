02b173d738e3aa21cfdb2eae148e9b8a7ce8c9cc
NelsonWamalwa <wamalwanelson@gmail.com>
2024-04-25 16:33:29 +0300

    Task 0 correction

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
---A script that creates a table users with follwing fields
--- id, email and name
+-- createing a 'users' table if it doesn't already exist.
+
 CREATE TABLE IF NOT EXISTS users (
-	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	email varchar(255) NOT NULL UNIQUE,
-	name varchar(255)
-)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
