827ceba1348d2bbfc5f7d92390cc03ed27e9791c
NelsonWamalwa <wamalwanelson@gmail.com>
2024-04-30 23:55:20 +0300

    web.py task

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''Implementing an expiring web cache and tracker
+'''
+import redis
+import requests
+from datetime import timedelta
+
+
+def get_page(url: str) -> str:
+    '''This returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    if url is None or len(url.strip()) == 0:
+        return ''
+    redis_store = redis.Redis()
+    res_key = 'result:{}'.format(url)
+    req_key = 'count:{}'.format(url)
+    result = redis_store.get(res_key)
+    if result is not None:
+        redis_store.incr(req_key)
+        return result
+    result = requests.get(url).content.decode('utf-8')
+    redis_store.setex(res_key, timedelta(seconds=10), result)
+    return result
